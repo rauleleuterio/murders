d1c23248c9b22c6539199c0dfc4e1d0c52a0c534
Raul Eleuterio <rauleleuterio93@gmail.com>
2020-04-15 22:25:58 -0300

    second try

---
+++ download-dat.R
@@ -0,0 +1,5 @@
+url <- "https://github.com/rafalab/dslabs/blob/master/inst/extdata/murders.csv"
+dest_file <- "data/murders.cvs"
+download.file(url, destfile = dest_file)
+
+
---
+++ wrangle-data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+murders <- read_csv("data/murders.cvs")
+murders <- murders %>% mutate(region = factor(region), rate = total / population * 10^5)
+save(murders, file = "rda/murders.rda")
+
+
+
